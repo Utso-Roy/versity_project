de84ff876fc1a00d6b03a2a46ab0f81bd547133e
Utso Roy <royutso003@gmail.com>
2025-12-02 22:45:35 +0600

    remove add product route to sidebar

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -6,17 +6,14 @@ import { Outlet } from "react-router";
 const RootLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Sticky Navbar */}
       <header className="sticky top-0 z-50 bg-white shadow-md">
         <Navbar />
       </header>
 
-      {/* Main content */}
       <main className="flex-grow">
         <Outlet />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
